cb866eb6c08ace0f1ca44f428cfd08b91e92b503
EmmaElizabeth30 <144685453+EmmaElizabeth30@users.noreply.github.com>
2024-09-25 14:38:23 +0100

    Add files via upload

---
+++ Project5_Coercion/main.js
@@ -0,0 +1,36 @@
+//displaying the type of data
+document.write(typeof 5);
+
+//type coercion
+document.write("15" + 5);
+
+//Boolean logic
+document.write(1>7);
+document.write(12===12);
+
+//console.log 
+console.log(4+4);
+console.log(1>7);
+
+//equality operators
+document.write(10==10);
+document.write(5==2);
+
+//triple equal operators
+document.write(10===10);
+document.write(5===2);
+document.write("100"==="100");
+document.write(6==="6");
+
+//ogical AND operator
+document.write(2<4 && 3<4);
+document.write(2>4 && 3>4);
+
+//logical OR operator
+document.write(5>3 || 6<4);
+document.write(1>4 || 3>4);
+
+//not operator
+document.write(!(2>3));
+document.write(!(1===1));
+
